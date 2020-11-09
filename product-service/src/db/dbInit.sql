debf249d450e5dbf88105305d8f53c4b43c70d60
Daria Nikiforova <darya.nikiforova94@gmail.com>
2020-11-09 19:02:19 +0300

    [feature/task4] added db init

---
+++ product-service/src/db/dbInit.sql
@@ -0,0 +1,27 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE products (
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	title text NOT NULL,
+	description text,
+	price integer
+);
+
+CREATE TABLE stock (
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	product_id uuid,
+	count integer,
+	FOREIGN KEY ("product_id") REFERENCES "product" ("id") ON DELETE CASCADE
+);
+
+INSERT INTO products (title, description, price) VALUES
+    ('Cat', 'so sweet', 25),
+    ('Dog', 'so funny', 45),
+    ('Mouse', 'so nice', 20),
+    ('Rabbit', 'so gray', 10)
+
+INSERT INTO stock (product_id, count) VALUES
+    ((SELECT id FROM product WHERE title = 'Cat'), 10),
+    ((SELECT id FROM product WHERE title = 'Dog'), 5),
+    ((SELECT id FROM product WHERE title = 'Mouse'), 5),
+    ((SELECT id FROM product WHERE title = 'Rabbit'), 10),
